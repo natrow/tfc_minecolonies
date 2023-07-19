2847cf08d866be83dd94eab22cf35bf4626bc80c
KGB-8375 <27stopittj@gmail.com>
2023-07-19 12:29:33 -0400

    Placeholder grass visual bug
    Part of #38.
    
     - Added proper render handling for grass-type placeholders

--- src/main/java/com/natrow/tfc_minecolonies/client/TFCMClientEventHandler.java
+++ src/main/java/com/natrow/tfc_minecolonies/client/TFCMClientEventHandler.java
@@ -20,6 +20,7 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 import net.dries007.tfc.client.TFCColors;
+import net.dries007.tfc.common.blocks.soil.SoilBlockType;
 import net.dries007.tfc.common.blocks.wood.Wood;
 
 public final class TFCMClientEventHandler
@@ -35,8 +36,15 @@ public final class TFCMClientEventHandler
     public static void clientSetup(FMLClientSetupEvent event)
     {
         event.enqueueWork(() -> {
-            Stream.of(Wood.BlockType.SAPLING, Wood.BlockType.DOOR, Wood.BlockType.TRAPDOOR, Wood.BlockType.FENCE, Wood.BlockType.FENCE_GATE, Wood.BlockType.BUTTON, Wood.BlockType.PRESSURE_PLATE, Wood.BlockType.SLAB, Wood.BlockType.STAIRS, Wood.BlockType.TWIG, Wood.BlockType.BARREL, Wood.BlockType.SCRIBING_TABLE, Wood.BlockType.POTTED_SAPLING).forEach(type -> ItemBlockRenderTypes.setRenderLayer(TFCMBlocks.PLACEHOLDER_WOODS.get(type).get(), RenderType.cutout()));
-            Stream.of(Wood.BlockType.LEAVES, Wood.BlockType.FALLEN_LEAVES).forEach(type -> ItemBlockRenderTypes.setRenderLayer(TFCMBlocks.PLACEHOLDER_WOODS.get(type).get(), layer -> Minecraft.useFancyGraphics() ? layer == RenderType.cutoutMipped() : layer == RenderType.solid()));
+            Stream.of(Wood.BlockType.SAPLING, Wood.BlockType.DOOR, Wood.BlockType.TRAPDOOR, Wood.BlockType.FENCE, Wood.BlockType.FENCE_GATE, Wood.BlockType.BUTTON, Wood.BlockType.PRESSURE_PLATE, Wood.BlockType.SLAB, Wood.BlockType.STAIRS, Wood.BlockType.TWIG, Wood.BlockType.BARREL, Wood.BlockType.SCRIBING_TABLE, Wood.BlockType.POTTED_SAPLING)
+                .forEach(type -> ItemBlockRenderTypes.setRenderLayer(TFCMBlocks.PLACEHOLDER_WOODS.get(type)
+                    .get(), RenderType.cutout()));
+            Stream.of(Wood.BlockType.LEAVES, Wood.BlockType.FALLEN_LEAVES)
+                .forEach(type -> ItemBlockRenderTypes.setRenderLayer(TFCMBlocks.PLACEHOLDER_WOODS.get(type)
+                    .get(), layer -> Minecraft.useFancyGraphics() ? layer == RenderType.cutoutMipped() : layer == RenderType.solid()));
+            Stream.of(SoilBlockType.GRASS, SoilBlockType.CLAY_GRASS)
+                .forEach(type -> ItemBlockRenderTypes.setRenderLayer(TFCMBlocks.PLACEHOLDER_SOIL.get(type)
+                    .get(), RenderType.cutoutMipped()));
         });
     }
 
@@ -44,8 +52,15 @@ public final class TFCMClientEventHandler
     {
         final BlockColors registry = event.getBlockColors();
         final BlockColor foliageColor = (state, level, pos, tintIndex) -> TFCColors.getFoliageColor(pos, tintIndex);
+        final BlockColor grassColor = (state, level, pos, tintIndex) -> TFCColors.getGrassColor(pos, tintIndex);
 
-        registry.register(foliageColor, TFCMBlocks.PLACEHOLDER_WOODS.get(Wood.BlockType.LEAVES).get(), TFCMBlocks.PLACEHOLDER_WOODS.get(Wood.BlockType.FALLEN_LEAVES).get());
+        registry.register(foliageColor, TFCMBlocks.PLACEHOLDER_WOODS.get(Wood.BlockType.LEAVES)
+            .get(), TFCMBlocks.PLACEHOLDER_WOODS.get(Wood.BlockType.FALLEN_LEAVES)
+            .get());
+
+        registry.register(grassColor, TFCMBlocks.PLACEHOLDER_SOIL.get(SoilBlockType.GRASS)
+            .get(), TFCMBlocks.PLACEHOLDER_SOIL.get(SoilBlockType.CLAY_GRASS)
+            .get());
     }
 
     public static void colorHandlerItems(ColorHandlerEvent.Item event)
@@ -53,22 +68,27 @@ public final class TFCMClientEventHandler
         final ItemColors registry = event.getItemColors();
         final ItemColor foliageColor = (stack, tintIndex) -> TFCColors.getFoliageColor(null, tintIndex);
 
-        registry.register(foliageColor, TFCMBlocks.PLACEHOLDER_WOODS.get(Wood.BlockType.FALLEN_LEAVES).get(), TFCMBlocks.PLACEHOLDER_WOODS.get(Wood.BlockType.LEAVES).get());
+        registry.register(foliageColor, TFCMBlocks.PLACEHOLDER_WOODS.get(Wood.BlockType.FALLEN_LEAVES)
+            .get(), TFCMBlocks.PLACEHOLDER_WOODS.get(Wood.BlockType.LEAVES)
+            .get());
     }
 
     public static void onTextureSwitch(TextureStitchEvent.Pre event)
     {
-        final ResourceLocation sheet = event.getAtlas().location();
+        final ResourceLocation sheet = event.getAtlas()
+            .location();
         if (sheet.equals(Sheets.CHEST_SHEET))
         {
-            Arrays.stream(TFCMWood.VALUES).map(TFCMWood::getSerializedName).forEach(name -> {
-                event.addSprite(TFCMConstants.getResourceLocation("entity/chest/normal/" + name));
-                event.addSprite(TFCMConstants.getResourceLocation("entity/chest/normal_left/" + name));
-                event.addSprite(TFCMConstants.getResourceLocation("entity/chest/normal_right/" + name));
-                event.addSprite(TFCMConstants.getResourceLocation("entity/chest/trapped/" + name));
-                event.addSprite(TFCMConstants.getResourceLocation("entity/chest/trapped_left/" + name));
-                event.addSprite(TFCMConstants.getResourceLocation("entity/chest/trapped_right/" + name));
-            });
+            Arrays.stream(TFCMWood.VALUES)
+                .map(TFCMWood::getSerializedName)
+                .forEach(name -> {
+                    event.addSprite(TFCMConstants.getResourceLocation("entity/chest/normal/" + name));
+                    event.addSprite(TFCMConstants.getResourceLocation("entity/chest/normal_left/" + name));
+                    event.addSprite(TFCMConstants.getResourceLocation("entity/chest/normal_right/" + name));
+                    event.addSprite(TFCMConstants.getResourceLocation("entity/chest/trapped/" + name));
+                    event.addSprite(TFCMConstants.getResourceLocation("entity/chest/trapped_left/" + name));
+                    event.addSprite(TFCMConstants.getResourceLocation("entity/chest/trapped_right/" + name));
+                });
         }
     }
 }
